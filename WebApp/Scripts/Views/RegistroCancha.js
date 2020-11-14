94fd09301073f041559378f6a716f26d487069b4
Tray <tmorac@ucenfotec.ac.cr>
2020-11-14 01:17:02 -0600

    Cambios formulario

--- WebApp/Scripts/Views/RegistroCancha.js
+++ WebApp/Scripts/Views/RegistroCancha.js
@@ -24,9 +24,9 @@
 	this.Registro = function () {
 		this.ctrlActions = new ControlActions();
 		var customerData = {};
-		//customerData = this.ctrlActions.GetDataForm('frmEdition');
-		customerData.name = "Paola Mora";
-		customerData.phone = 89696205;
+		customerData = this.ctrlActions.GetDataForm('frmEdition');
+		//customerData.name = "Paola Mora";
+		//customerData.phone = 89696205;
 		this.ctrlActions.PostToAPI(this.service, customerData);
 		var e = document.getElementById("fecha").value;
 		console.log(e);
